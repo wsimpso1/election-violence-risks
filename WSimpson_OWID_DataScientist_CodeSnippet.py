befc1c94ca25e463d8f46c3031e33753c6f37927
wsimpso1 <54618026+wsimpso1@users.noreply.github.com>
2023-02-09 14:27:03 -0500

    update data formatting

--- WSimpson_OWID_DataScientist_CodeSnippet.py
+++ WSimpson_OWID_DataScientist_CodeSnippet.py
@@ -18,17 +18,21 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classifica
 from sklearn.model_selection import GridSearchCV
 
 
+
 # load data
 
 # data directory
 data_dir = 'https://raw.githubusercontent.com/wsimpso1/election-violence-risks/main/data'
-
+# dataframes
 deco = pd.read_csv(data_dir+'/DECO_v.1.0.csv', parse_dates=['date_start','date_end'])
 nelda = pd.read_csv(data_dir+'/NELDA.csv', encoding='latin-1')
 nelda_look_up = pd.read_csv(data_dir+'/nelda_look_up.csv', encoding='latin-1')
 
-# datetime conversion
 
+
+# preprocessing
+
+# datetime conversion
 def custom_nelda_date_format(year, mmdd):
     '''
     parse year and month-day columns for NELDA data
@@ -49,12 +53,13 @@ def custom_nelda_date_format(year, mmdd):
     # impute date for missing data 
     except:
         return datetime.strptime(f'{year}0101', '%Y%m%d').date()
-        
+
+# apply function        
 nelda['date'] = pd.to_datetime([custom_nelda_date_format(year,date) for year, date in zip(nelda.year,nelda.mmdd)])
 
 
-# clean NELDA data of historical elections for modeling
 
+# clean NELDA data of historical elections for modeling
 def process_nelda(nelda_data):
     '''
     data wrangling for NELDA data
@@ -107,11 +112,11 @@ def process_nelda(nelda_data):
     return nelda_data.reset_index(drop=True)
 
 # apply cleaning function
-
 nelda_clean = process_nelda(nelda)
 
-# clean data of election violence (DECO) for modeling
 
+
+# clean data of election violence (DECO) for modeling
 def process_deco(deco_data):
     '''
     data wrangling for DECO data
@@ -138,11 +143,11 @@ def process_deco(deco_data):
     return deco_data.reset_index()
 
 # apply cleaning function
-
 deco_agg = process_deco(deco)
 
-# 3.2 Merge Preprocessed and Aggregated NELDA and DECO datasets
 
+
+# Merge Preprocessed and Aggregated NELDA and DECO datasets
 def fatalities_per_election(election_date, country_id):
     '''
     compute the total number of election related fatalities in 1 year leading up to election date
@@ -166,42 +171,34 @@ def fatalities_per_election(election_date, country_id):
     sum_election_fatalities = deco_agg_country_1_year.num_fatalities.sum()
     return sum_election_fatalities
 
+# apply function
 deco_election_fatalities = [fatalities_per_election(date, country) for date, country in zip(nelda_clean.date, nelda_clean.ccode)]
 
 # create final combined dataframe
-
 nelda_deco = nelda_clean.copy()
 nelda_deco['election_fatalities'] = deco_election_fatalities
 
 
-# Drop unecessary columns 
 
+# Drop unecessary columns 
 drop_cols = ['stateid','ccode', 'country', 'electionid', 'year', 'mmdd', 'types', 'notes', 'date']
 nelda_deco = nelda_deco.drop(drop_cols, axis=1)
 
-
-# view combined data that will be used in modeling
-
-# all NELDA variables will be used as independent variables to predict the dependent variable election_fatalities
-# which is the best estimate of election related fatalities for that country-year.
-
+# reset index
 nelda_deco.reset_index(inplace=True, drop=True)
 
-# ### 3.4. Remove Multicolinearity
-
+# Remove Multicolinearity
 # of the highly correlated features keep those that are most potentially informative
-
 nelda_deco = nelda_deco.drop(columns=['nelda8','nelda21', 'nelda29',
                                                 'nelda36', 'nelda37',
                                                 'nelda40', 'nelda41'])
 
 
-# # 4. Model Building
 
-# ### 4.1 Define features and target variable 
+
+# Model Building
 
 # convert target variable of fatalities to a categorical variable
-
 def to_categorial(val):
     '''
     Discretizes numerical value of fatalities in a country-year
@@ -227,33 +224,25 @@ def to_categorial(val):
         return 'mass fatality'
 
 # apply categorical conversion function
-
 nelda_deco['election_fatalities'] = [to_categorial(row) for row in nelda_deco.election_fatalities]
 
-
 # define target variable y (election fatalities) and features X (risk factors) 
-
 y = nelda_deco.election_fatalities
 X = nelda_deco.drop(['election_fatalities'], axis=1)
-
 
 # Drop columns that contain information about election fatalities the model may cheat on
 # - nelda 33 explicitly codes for the presence of fatalities
 # - nelda 31 codes for the use of violence by the government against citizens
-
-# drop columns as described above
-
 X = X.drop(columns=['nelda33', 'nelda31'])
 
-
 # transform all categorical NELDA features to one-hot encoding
-
 # Creates list of all column headers
 all_columns = list(X)
 # change datatype
 X[all_columns] = X[all_columns].astype(str)
 # one hot encoding
 X_one = pd.get_dummies(X)
+
 
 
 # Adjust class imblance to mitigate overfitting via under/oversampling
@@ -267,8 +256,6 @@ oversample = RandomOverSampler(sampling_strategy='all', random_state=42)
 X_over, y_over = oversample.fit_resample(X_under, y_under)
 
 
-# ### 4.2 Train Test Split
-
 
 # split data into training and testing sets
 
@@ -279,10 +266,10 @@ X_train, X_test, y_train, y_test = train_test_split(X_over,
                                                     stratify=y_over)
 
 
-# ### 4.3 Train Model and Search for Optimal Parameters
+
+# Train Model and Search for Optimal Parameters
 
 # define grid search and cross validation
-
 parameter_grid = {'n_estimators':[50, 100, 200, 300, 500, 800, 1000],
               'max_depth':[3, 4, 5, 7], 
               'criterion': ['gini', 'entropy']}
@@ -291,45 +278,32 @@ parameter_grid = {'n_estimators':[50, 100, 200, 300, 500, 800, 1000],
 rf_model = RandomForestClassifier(random_state=42)
 rf_grid_cv = GridSearchCV(rf_model, parameter_grid, verbose=1, cv=5)
 
-
 # train
-
 rf_grid_cv.fit(X_train, y_train)
 
 # best parameters
 print('Best Parameters:', rf_grid_cv.best_params_)
 
 # get predictions with best model
-
 y_pred = rf_grid_cv.predict(X_test)
 
-
-
 # training accuracy of best model
-
 print('Train Accuracy:', round(rf_grid_cv.score(X_train, y_train)*100, 2), '%')
 
-
 # testing accuracy of best model
-
 print('Test Accuracy:', round(rf_grid_cv.score(X_test, y_test)*100, 2), '%')
 
-
-
 # examine precision and recall from classification report
-
 clf_rpt = classification_report(y_test, y_pred, target_names=rf_grid_cv.classes_)
 print(clf_rpt)
 
 
-# ### 4.4 Feature Importance
 
 # determine feature importances 
-
 '''
 Permutation Importance: 
 A strategy to measure the decrease in model performance as the result of 
-randomly shuffling one feature at a time. More important features in the model’s final decision 
+randomly shuffling one feature at a time. More important features in the model's final decision 
 cause a larger drop in performance when shuffled.
 '''
 
@@ -342,25 +316,19 @@ perm_optimized['AVG_Importance'] = r.importances_mean
 
 perm_optimized = perm_optimized.sort_values('AVG_Importance', ascending=False)
 
-
 # select features with importance above threshold
-
-importance_threshold = 0.005
+importance_threshold = 0.004
 perm_optimized = perm_optimized.loc[perm_optimized.AVG_Importance > importance_threshold]
 
 
-# # 5. Obtain Top Risk Factors of Historical Election Violence 
 
 # get top most important risk factors to predicting historical election violence 
-
 top_nelda_codes = [re.findall(r'nelda\d{1,2}',row) for row in perm_optimized.index]
 top_nelda_codes = pd.DataFrame(np.concatenate(top_nelda_codes), columns=['nelda_feature'])
 
 top_unique_nelda_codes = list(top_nelda_codes.nelda_feature.unique())
 
-
 # look up text descriptions of top NELDA risk factors
-
 top_nelda_code_descriptions = nelda_look_up.loc[nelda_look_up.nelda_code.isin(top_unique_nelda_codes)][['nelda_code', 'description_clean']]
 top_nelda_code_descriptions = top_nelda_code_descriptions.rename(columns={'description_clean':'election_characteristic'})
 top_nelda_code_descriptions.reset_index(inplace=True, drop=True)
